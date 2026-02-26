f50b691a4bce36ef172c491eb6e94cf768517d6c
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-02-25 18:29:55 -0600

    Initial Commit

---
+++ SwiftUI-Camera/SwiftUI_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CameraApp.swift
+//  SwiftUI-Camera
+//
+//  Created by Dmytro Maksiutenko on 2026-02-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-CameraTests/SwiftUI_CameraTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CameraTests.swift
+//  SwiftUI-CameraTests
+//
+//  Created by Dmytro Maksiutenko on 2026-02-25.
+//
+
+import Testing
+@testable import SwiftUI_Camera
+
+struct SwiftUI_CameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
